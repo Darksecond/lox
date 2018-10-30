9019ec5a72e87782ddb536d02d28bda8dc1e8b73
Tim Peters <mail@darksecond.nl>
2018-10-30 21:10:36 +0100

    Refactor Bytecode

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -1,18 +1,34 @@
-pub enum Object {
-    String(String),
-}
-pub enum Value {
-    Number(f64),
-    Boolean(bool),
-    Nil,
-    Object(Object),
-}
+pub type InstructionIndex = usize;
+pub type ConstantIndex = usize;
 
-pub enum Operator {
-    Constant(Value),
+pub enum Instruction {
+    Constant(ConstantIndex),
     Negate,
     Add,
     Subtract,
     Multiply,
     Divide,
+    // etc
+}
+
+pub enum Constant {
+    Number(f64),
+    String(String),
+}
+
+pub struct Chunk {
+    instructions: Vec<Instruction>,
+    constants: Vec<Constant>,
+}
+
+impl Chunk {
+    pub fn add_instruction(&mut self, instruction: Instruction) -> InstructionIndex{
+        self.instructions.push(instruction);
+        self.instructions.len() - 1
+    }
+
+    pub fn add_constant(&mut self, constant: Constant) -> ConstantIndex {
+        self.constants.push(constant);
+        self.constants.len() - 1
+    }
 }
