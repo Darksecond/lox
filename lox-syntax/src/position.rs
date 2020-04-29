27381e5d24cd604a4bf14ae71d1631bf6c13b346
Tim Peters <mail@darksecond.nl>
2020-04-29 17:09:46 +0200

    Remove position entirely for now

--- lox-syntax/src/position.rs
+++ lox-syntax/src/position.rs
@@ -7,44 +7,6 @@ impl BytePos {
     }
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Eq, Ord)]
-pub struct Position {
-    pub line: usize,
-    pub column: usize,
-}
-
-impl Default for Position {
-    fn default() -> Self {
-        Position { line: 1, column: 1, }
-    }
-}
-
-impl Position {
-    fn increment_column(&mut self) {
-        self.column += 1;
-    }
-
-    fn increment_line(&mut self) {
-        self.column = 1;
-        self.line += 1;
-    }
-
-    pub fn shift(mut self, ch: char) -> Self {
-        if ch == '\n' {
-            self.increment_line();
-        } else {
-            self.increment_column();
-        }
-        self
-    }
-
-    pub fn unshift(mut self) -> Self {
-        self.column -= 1;
-        if self.column <= 0 { panic!("Cannot unshift onto previous line"); }
-        self
-    }
-}
-
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Span {
     pub start: BytePos,
