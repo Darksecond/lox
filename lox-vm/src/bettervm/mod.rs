e6bda819e72e981d9835da9253086e301c15f146
Tim Peters <mail@darksecond.nl>
2021-03-13 14:31:21 +0100

    Do minor cleanup

--- lox-vm/src/bettervm/mod.rs
+++ lox-vm/src/bettervm/mod.rs
@@ -1,16 +1,14 @@
 mod memory;
-mod vm;
+pub mod vm;
 
 use crate::bytecode::Module;
 use vm::Vm;
 
 pub use vm::VmError;
 
-pub fn execute(module: &Module) -> Result<(), VmError> {
-    let mut vm = Vm::new(module);
-
-    //TODO Work on a way of initializing and executing the VM.
-    //     It should be possible to define your own native functions.
+/// Add the lox standard library to a Vm instance.
+/// Right now the stdlib consists of 'clock'.
+pub fn set_stdlib(vm: &mut Vm) {
     vm.set_native_fn("clock", |_args| {
         use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -20,8 +18,11 @@ pub fn execute(module: &Module) -> Result<(), VmError> {
             .as_secs_f64();
         memory::Value::Number(time)
     });
+}
 
-    vm.interpret()?;
+pub fn execute(module: &Module) -> Result<(), VmError> {
+    let mut vm = Vm::new(module);
+    set_stdlib(&mut vm);
 
-    Ok(())
+    vm.interpret()
 }
